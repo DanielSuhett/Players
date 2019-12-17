9e1aaea75dc92b70c8d42020a1ef2da7f914db2d
DanielSuhett <danysuhett@hotmail.com>
2019-12-16 23:46:00 -0300

    implementação de cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,9 +1,9 @@
 const bodyParser = require("body-parser");
 const logger = require("morgan");
-
+const cors = require("cors")
 module.exports = app => {
   app.use(bodyParser.json());
- 
+  app.use(cors())
   app.use(logger("dev"));
 
 }
