56a1857a54bbdaf93855662fe4c33147064a7790
Suhett <danysuhett@hotmail.com>
2019-12-24 20:38:28 -0300

    Elevação de state e feature de edição completa

--- public/src/components/List.js
+++ public/src/components/List.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
 import API from '../services/api';
-import ArrowDown from '/home/devsuhett/Projects/Players/public/images/arrow-down.png'
-import ArrowUp from '/home/devsuhett/Projects/Players/public/images/arrow-up.png'
-import Trash from '/home/devsuhett/Projects/Players/public/images/trash.png'
-import Edit from '/home/devsuhett/Projects/Players/public/images/edit.png'
-import Confirm from '/home/devsuhett/Projects/Players/public/images/confirm.png'
-import Delete from '/home/devsuhett/Projects/Players/public/images/delete-white.png';
 
-let id = 0;
 export default class List extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: this.props.item.username,
-      game: '',
-      games: this.props.item.games,
-      buttonScope: false,
+      showGames: false,
       clickEdit: false
     }
   }
@@ -26,8 +16,9 @@ export default class List extends Component {
         {
           this.state.clickEdit
             ? <span>
-              <input key={id} className="gameItemInput" onChange={(e) => this.gameFieldChanged(index, e)} defaultValue={game} />
-              <button onClick={(e) => this.removeGameField(e, index)} className="buttonItemList"><img alt="delete game of player" src={Delete} />
+              <input className="gameItemInput" onChange={(e) => this.props.updateGame(index, e)} value={game} />
+              <button onClick={(e) => this.props.removeGameField(index, e)} className="buttonItemList">
+                <img alt="delete game of player" src="img/delete-white.png" />
               </button>
             </span>
             : <div key={game} className="gameItem">{game}</div>
@@ -36,33 +27,11 @@ export default class List extends Component {
     );
   }
 
-  gameFieldChanged(index, e) {
-    this.setState({ games: this.state.games.reduce((game, new_game) => 
-      { new_game = e.target.value; return game + new_game }) 
-    })
-    console.log(this.state);
-  }
-
-  removeGameField(e, index) {
-    e.preventDefault();
-    const new_games = this.state.games.filter((s, gindex) => { return index !== gindex })
-    if (new_games.length)
-      this.setState({ games: new_games });
-  }
-
-  buttonToggle = () => {
-    this.setState({ buttonScope: !this.state.buttonScope })
-  }
-
-  async submitEdit(playerid, new_data) {
-    try {
-      await API.put(`players/${playerid}`, new_data)
-      this.props.handleUpdateList()
+  buttonToggle = (button) => {
+    if (button == 'showGames')
+      this.setState({ showGames: !this.state.showGames });
+    if (button == 'clickEdit')
       this.setState({ clickEdit: !this.state.clickEdit })
-
-    } catch (error) {
-      console.log(error);
-    }
   }
 
   removePlayer = (playerid) => {
@@ -71,47 +40,51 @@ export default class List extends Component {
   }
 
   render() {
-    const { buttonScope, games } = this.state;
+    const { showGames } = this.state;
     const { item } = this.props;
     let imageurl;
     return (
       <div className="itemBox">
         <div className="itemList">
           {this.state.clickEdit
-            ? <input className="inputEditName" onChange={(e) => this.setState({ username: e.target.value })} defaultValue={item.username} />
+            ? <input className="inputEditName" onChange={(e) => this.props.updatePlayerName(e)} value={item.username} />
             : <li>{item.username}
-              <button className="buttonItemList" onClick={() => { this.removePlayer(item._id) }}>
-                <img alt="remove player" src={Trash} />
-              </button>
-              <button className="buttonItemList" onClick={() => { this.setState({ clickEdit: !this.state.clickEdit }) }} >
-                <img alt="edit player" src={Edit} />
-              </button>
-              <button className="buttonItemList" onClick={this.buttonToggle.bind(this)}>
-                <span className="hiddenButtonText">{buttonScope ? imageurl = ArrowDown : imageurl = ArrowUp}</span>
+
+              <button className="buttonItemList" onClick={() => this.buttonToggle('showGames')}>
+                <span className="hiddenButtonText">{showGames ? imageurl = 'img/arrow-down.png' : imageurl = 'img/arrow-up.png'}</span>
                 <img alt="arrow to display items" src={imageurl} />
               </button>
+
+              <button className="buttonItemList" onClick={() => { this.removePlayer(item._id) }}>
+                <img alt="remove player" src="img/trash.png" />
+              </button>
+              {
+                showGames
+                  ? <button className="buttonItemList" onClick={() => { this.buttonToggle('clickEdit') }} >
+                    <img alt="edit player" src="img/edit.png" />
+                  </button>
+                  : null
+              }
             </li>
           }
           <div>
             {
-              buttonScope
+              showGames
                 ? <div className="gameList">
-                  {games.map((g, index) => {
+                  {item.games.map((g, index) => {
                     return <span key={index}>{this.games(index, g)}</span>
                   })
                   }
                   {this.state.clickEdit
                     ? <span className="ButtonEditList">
                       <button onClick={() => {
-                        this.submitEdit(item._id, {
-                          username: this.state.username,
-                          games: this.state.games
-                        });
+                        this.props.updateSubmit();
+                        this.buttonToggle('clickEdit');
                       }}>
-                        <img alt="confirm edit" src={Confirm}></img>
+                        <img alt="confirm edit" src="img/confirm.png"></img>
                       </button>
-                      <button onClick={() => { this.setState({ clickEdit: !this.state.clickEdit }) }}>
-                        <img alt="cancel edit" src={Delete}></img>
+                      <button onClick={() => { this.buttonToggle('clickEdit'); this.props.handleUpdateList() }}>
+                        <img alt="cancel edit" src="img/delete-white.png"></img>
                       </button>
                     </span>
                     : null
--- public/src/pages/Home.js
+++ public/src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import '../../css/home.css';
+import '../../public/css/home.css';
 import List from '../components/List'
 import NewPlayer from '../components/NewPlayer';
 import { isAuth } from '../services/auth';
@@ -13,13 +13,71 @@ export default class Home extends Component {
     super(props);
     this.state = {
       data: [],
+      dataUpdate: [],
       redirect: false
     };
   };
 
   async getPlayers() {
     const res = await API.get('/players');
-      this.setState({ data: res.data });
+    this.setState({ data: res.data });
+  }
+
+  async updateSubmit() {
+    try {
+      for(const updatePayload of this.state.dataUpdate){
+        await API.put(`players/${updatePayload._id}`, updatePayload)
+      }
+      this.getPlayers()
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+  updatePlayerName(indexItem, e) {
+    const { value } = e.target
+    this.setState(state => ({
+      ...state,
+      dataUpdate: state.data.map((item, idx) => {
+        indexItem === idx
+          ? item.username = value
+          : item
+
+        return item
+      })
+    }))
+
+  }
+
+  updateGame(indexItem, game_id, e) {
+    const { value } = e.target
+
+    this.setState(state => ({
+      ...state,
+      dataUpdate: state.data.map((item, idx) => {
+        indexItem === idx
+          ? item.games[game_id] = value
+          : item
+
+        return item
+      })
+    }))
+  }
+
+  removeGameField(indexItem, game_id, e) {
+    e.preventDefault();
+
+    this.setState(state => ({
+      ...state,
+      dataUpdate: state.data.map((item, idx) => {
+        indexItem === idx
+          ? item.games = item.games.filter((g, gindex) => { return game_id !== gindex })
+          : item
+
+        return item
+      })
+    }))
   }
 
 
@@ -36,9 +94,16 @@ export default class Home extends Component {
             {
               this.state.data.length
                 ?
-
                 this.state.data.map((item, index) => {
-                  return <List handleUpdateList={() => this.getPlayers()}  key={index} item={item} />
+                  return <List
+                    handleUpdateList={() => this.getPlayers()}
+                    updateGame={(game_id, e) => this.updateGame(index, game_id, e)}
+                    removeGameField={(game_id, e) => this.removeGameField(index, game_id, e)}
+                    updatePlayerName={(e) => this.updatePlayerName(index, e)}
+                    updateSubmit={(_id) => this.updateSubmit(_id)}
+                    key={index}
+                    item={item}
+                  />
                 })
                 : null
             }
